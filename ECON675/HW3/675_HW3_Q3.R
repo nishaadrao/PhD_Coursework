8c56a0fa0a21ee9e64b5888ba02b0092bf9a70c2
Ani <asyadav@umich.edu>
2018-10-26 11:49:31 -0400

    finished q3 writeup + R

--- ECON675/HW3/675_HW3_Q3.R
+++ ECON675/HW3/675_HW3_Q3.R
@@ -9,6 +9,7 @@
 rm(list = ls())             #clear workspace
 library(foreach)            #for looping
 library(data.table)         #for data manipulation
+library(dplyr)              #melting for ggplot
 library(Matrix)             #fast matrix calcs
 library(ggplot2)            #for pretty plots
 library(sandwich)           #for variance-covariance estimation 
@@ -18,7 +19,7 @@ options(scipen = 999)       #forces R to use normal numbers instead of scientifi
 
 
 ######################################################################
-# Q1: Simulate data, run bootsrap
+# Q1: Nonparametric bootstrap fail
 ######################################################################
 set.seed(123)
 
@@ -38,4 +39,30 @@ boot.stat = function(data, i){
 # Run bootsrap with 599 replications
 boot.results = boot(data = X, R = 599, statistic = boot.stat)
 
-# NEXT UP PLOT THE BOOT HISTOGRAM WITH EXP(1) OVERLAYED!
+# Make frequency plot
+h         = hist(boot.results$t,plot=FALSE)
+h$density = h$counts/sum(h$counts)
+plot(h,freq=FALSE,main="Distribution of Bootstrap Statistic",xlab="Bootstrap statistic")
+
+
+######################################################################
+# Q2: Parametric bootstrap fail
+######################################################################
+
+# Generate parametric bootstrap samples
+X.boot = replicate(599,runif(N,0,x.max.obs))
+
+# Compute maximums for each replications
+x.max.boot = sapply(1:599,function(i) max(X.boot[,i]))
+
+# Compute bootstrap statistic
+t.boot     = N*(x.max.obs-x.max.boot)
+
+# Make frequency plot
+h2         = hist(t.boot,plot=FALSE)
+h2$density = h2$counts/sum(h2$counts)
+plot(h2,freq=FALSE,main="Distribution of Parametric Bootstrap Statistic",xlab="Parametric bootstrap statistic",ylim=c(0,0.4))
+
+
+
+
