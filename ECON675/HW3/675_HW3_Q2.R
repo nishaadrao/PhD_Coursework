b7d2775687f8296d700b90513ad28583fd9c159e
Ani <asyadav@umich.edu>
2018-10-29 12:37:04 -0400

    q4 complete

--- ECON675/HW3/675_HW3_Q2.R
+++ ECON675/HW3/675_HW3_Q2.R
@@ -70,6 +70,85 @@ for (i in 1:4) {
 rownames(table3)=c("dpisofirme", "S_age","S_HHpeople","log_inc")
 colnames(table3)=c("Estimate", "Std.Error", "t", "p-value", "CI.lower","CI.upper")
 
+######################################################################
+# Q3(c): Missing at random
+######################################################################
+# GMM moment condition
+g_MAR <- function(theta, data) {
+  data <- data[data$nmissing==1, ]
+  a <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$dpisofirme * data$weights
+  b <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$S_age * data$weights
+  c <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$S_HHpeople * data$weights
+  d <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    log(1+data$S_incomepc) * data$weights
+  cbind(a, b, c, d)
+}
 
+# logistic bootstrap
+boot.T_MAR <- function(boot.data, ind) {
+  data.temp <- boot.data[ind, ]
+  fitted <- glm(nmissing ~ dpisofirme + S_age + S_HHpeople +I(log(S_incomepc+1)) - 1, 
+                data = data.temp, 
+                family = binomial(link = "logit"))$fitted
+  data.temp$weights <- 1 / fitted
+  gmm(g_MAR, data.temp, t0=c(0,0,0,0), wmatrix="ident", vcov="iid")$coef
+}
+
+ptm <- proc.time()
+set.seed(123)
+temp <- boot(data=pisofirme, R=499, statistic = boot.T_MAR, stype = "i")
+proc.time() - ptm
+table5 <- matrix(NA, ncol=6, nrow=4)
+for (i in 1:4) {
+  table5[i, 1] <- temp$t0[i]
+  table5[i, 2] <- sd(temp$t[, i])
+  table5[i, 3] <- table5[i, 1] / table5[i, 2]
+  table5[i, 4] <- 2 * max( mean(temp$t[, i]-temp$t0[i]>=abs(temp$t0[i])), mean(temp$t[, i]-temp$t0[i]<=-1*abs(temp$t0[i])) )
+  table5[i, 5] <- 2 * temp$t0[i] - quantile(temp$t[, i], 0.975)
+  table5[i, 6] <- 2 * temp$t0[i] - quantile(temp$t[, i], 0.025)
+}
+
+######################################################################
+# Q3(d): Trimming
+######################################################################
+g_MAR2 <- function(theta, data) {
+  data <- data[data$nmissing==1 & data$weights<=1/0.1, ]
+  a <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$dpisofirme * data$weights
+  b <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$S_age * data$weights
+  c <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    data$S_HHpeople * data$weights
+  d <- (data$danemia - plogis(theta[1]*data$dpisofirme + theta[2]*data$S_age + theta[3]*data$S_HHpeople + theta[4]*log(1+data$S_incomepc))) * 
+    log(1+data$S_incomepc) * data$weights
+  cbind(a, b, c, d)
+}
+
+# logistic bootstrap
+boot.T_MAR2 <- function(boot.data, ind) {
+  data.temp <- boot.data[ind, ]
+  fitted <- glm(nmissing ~ dpisofirme + S_age + S_HHpeople +I(log(S_incomepc+1)) - 1, 
+                data = data.temp, 
+                family = binomial(link = "logit"))$fitted
+  data.temp$weights <- 1 / fitted
+  gmm(g_MAR2, data.temp, t0=c(0,0,0,0), wmatrix="ident", vcov="iid")$coef
+}
+
+ptm <- proc.time()
+set.seed(123)
+temp <- boot(data=pisofirme, R=499, statistic = boot.T_MAR2, stype = "i")
+proc.time() - ptm
+table6 <- matrix(NA, ncol=6, nrow=4)
+for (i in 1:4) {
+  table6[i, 1] <- temp$t0[i]
+  table6[i, 2] <- sd(temp$t[, i])
+  table6[i, 3] <- table6[i, 1] / table6[i, 2]
+  table6[i, 4] <- 2 * max( mean(temp$t[, i]-temp$t0[i]>=abs(temp$t0[i])), mean(temp$t[, i]-temp$t0[i]<=-1*abs(temp$t0[i])) )
+  table6[i, 5] <- 2 * temp$t0[i] - quantile(temp$t[, i], 0.975)
+  table6[i, 6] <- 2 * temp$t0[i] - quantile(temp$t[, i], 0.025)
+}
 
 
