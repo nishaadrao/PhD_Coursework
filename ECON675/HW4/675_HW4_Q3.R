03c8d2c15d6fb96045f1a3c474141c9679b71d57
Ani <asyadav@umich.edu>
2018-11-09 15:21:49 -0500

    q3 done

--- ECON675/HW4/675_HW4_Q3.R
+++ ECON675/HW4/675_HW4_Q3.R
@@ -18,7 +18,7 @@ library(mvtnorm)            #for MVN stuff
 options(scipen = 999)       #forces R to use normal numbers instead of scientific notation
 
 ######################################################################
-# Generate random data
+# Generate random data and simulate
 ######################################################################
 
 N     = 50
@@ -55,11 +55,6 @@ beta.sel    = ifelse(t.stats>=1.96,beta.hats,beta.tildes)
 # Summary statistics
 beta.sum = rbind(summary(beta.hats),summary(beta.tildes),summary(beta.sel))
 
-# Kernel density plots
-plot(density(beta.hats,kernel="e",bw="ucv",na.rm=TRUE),main="Empirical Densities of the Different Estimators")
-lines(density(beta.tildes,kernel="e",bw="ucv",na.rm=TRUE))
-lines(density(beta.sel,kernel="e",bw="ucv",na.rm=TRUE))
-
 # Make kernenl desity plot
 plot.dat = data.frame(beta = c(beta.hats,beta.tildes,beta.sel),Estimator=rep(c("hat", "tilde","sel"), each = M))
 
@@ -75,5 +70,31 @@ densplot = ggplot(plot.dat,aes(x=beta,fill=Estimator))+
                     labels=c("(i)", "(ii)", "(iii)"))+
              theme(legend.justification = c(0.05, 0.98), legend.position = c(0.05, 0.98))
 
+######################################################################
+# [2] Coverage rates
+######################################################################
 
+# Compute coverage rate for beta.hat
+beta.hats.se       = sapply(1:M,function(i) summary(lm(Y[,i]~W[,,i]))[["coefficients"]][, "Std. Error"][2])
+beta.hats.CIs      = cbind(beta.hats-1.96*beta.hats.se,beta.hats+1.96*beta.hats.se)
+beta.hats.covered  = ifelse(0.5>=beta.hats.CIs[,1]&0.5<=beta.hats.CIs[,2],1,0)
+beta.hat.cr        = mean(beta.hats.covered)
+
+# Compute coverage rate for beta.tilde
+beta.tildes.se       = sapply(1:M,function(i) summary(lm(Y[,i]~W[,1,i]))[["coefficients"]][, "Std. Error"][2])
+beta.tildes.CIs      = cbind(beta.tildes-1.96*beta.tildes.se,beta.tildes+1.96*beta.tildes.se)
+beta.tildes.covered  = ifelse(0.5>=beta.tildes.CIs[,1]&0.5<=beta.tildes.CIs[,2],1,0)
+beta.tilde.cr        = mean(beta.tildes.covered)
+
+# Compute coverage rate for beta.sel
+beta.sel.CI.lower    = ifelse(beta.hats==beta.sel,beta.hats-1.96*beta.hats.se,beta.tildes-1.96*beta.tildes.se)
+beta.sel.CI.upper    = ifelse(beta.hats==beta.sel,beta.hats+1.96*beta.hats.se,beta.tildes+1.96*beta.tildes.se)
+beta.sel.CIs         = cbind(beta.sel.CI.lower,beta.sel.CI.upper)
+beta.sel.covered     = ifelse(0.5>=beta.sel.CIs[,1]&0.5<=beta.sel.CIs[,2],1,0)
+beta.sel.cr          = mean(beta.sel.covered)
+
+# Put results together
+cr.results           = rbind(beta.hat.cr,beta.tilde.cr,beta.sel.cr)
+rownames(cr.results) = c("beta.hat.cr","beta.tilde.cr","beta.sel.cr")
+colnames(cr.results) = c("Coverage Rate")
 
