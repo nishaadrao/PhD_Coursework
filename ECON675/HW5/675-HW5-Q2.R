94cdb7705306c66cd21bf234aabd539902ac89d3
Ani <asyadav@umich.edu>
2018-11-19 14:26:00 -0500

    q2 done

--- ECON675/HW5/675-HW5-Q2.R
+++ ECON675/HW5/675-HW5-Q2.R
@@ -60,24 +60,43 @@ ols.rej  = ifelse(ols.t>1.96,1,0)
 ols.results = foreach(j=1:length(gamma.vec)) %do%
   rbind(c(mean(ols.beta[[j]]),sd(ols.beta[[j]]),quantile(ols.beta[[j]], probs = c(0.1, 0.5 ,0.9))),
   c(mean(ols.se[[j]]),sd(ols.se[[j]]),quantile(ols.se[[j]], probs = c(0.1, 0.5 ,0.9))),
-  c(mean(ols.rej[[j]]),sd(ols.rej[[j]]),quantile(ols.rej[[j]], probs = c(0.1, 0.5 ,0.9))))
+  c(mean(ols.rej[,j]),sd(ols.rej[,j]),quantile(ols.rej[,j], probs = c(0.1, 0.5 ,0.9))))
+
+# Remove big objects!
+rm(ols.big,ols.beta,ols.se,ols.t,ols.rej)
 
 ######################################################################
 # Compute 2SLS statistics for each gamma, and simulation
 ######################################################################
 
 # Run 2SLS for each gamma and each simulation -- this spits out 5000 ivreg's for each gamma
+# WATCH OUT: this takes a minute or so!
 iv.big = foreach(j=1:length(gamma.vec)) %do%
   lapply(1:M, function(i) ivreg(Y[,i]~X[[j]][,i]-1|W[,1,i]))
 
 # Extract point estimates, standard errors, t-stats
-ols.beta = foreach(j=1:length(gamma.vec)) %do%
-  sapply(1:M, function(i) ols.big[[j]][[i]]$coefficients)
+iv.beta = foreach(j=1:length(gamma.vec)) %do%
+  sapply(1:M, function(i) iv.big[[j]][[i]]$coefficients)
 
-ols.se   = foreach(j=1:length(gamma.vec)) %do%
-  sapply(1:M, function(i) coef(summary(ols.big[[j]][[i]]))[,"Std. Error"])
+iv.se   = foreach(j=1:length(gamma.vec)) %do%
+  sapply(1:M, function(i) summary(iv.big[[j]][[i]])[["coefficients"]][,"Std. Error"])
 
-ols.t    = sapply(1:length(gamma.vec),function(j) ols.beta[[j]]/ols.se[[j]])
+iv.t    = sapply(1:length(gamma.vec),function(j) iv.beta[[j]]/iv.se[[j]])
 
-ols.rej  = ifelse(ols.t>1.96,1,0)
+iv.rej  = ifelse(iv.t>1.96,1,0)
+
+# Run first-stage regression and extract F-statistics
+iv.f       = foreach(j=1:length(gamma.vec)) %do%
+  sapply(1:M, function(i) summary(lm(X[[j]][,i]~W[,1,i]-1))$fstatistic[1])
+
+# Combine results for each gamma
+iv.results = foreach(j=1:length(gamma.vec)) %do%
+  rbind(c(mean(iv.beta[[j]]),sd(iv.beta[[j]]),quantile(iv.beta[[j]], probs = c(0.1, 0.5 ,0.9))),
+        c(mean(iv.se[[j]]),sd(iv.se[[j]]),quantile(iv.se[[j]], probs = c(0.1, 0.5 ,0.9))),
+        c(mean(iv.rej[,j]),sd(iv.rej[,j]),quantile(iv.rej[,j], probs = c(0.1, 0.5 ,0.9))),
+        c(mean(iv.f[[j]]),sd(iv.f[[j]]),quantile(iv.f[[j]], probs = c(0.1, 0.5 ,0.9))))
+
+# Remove big objects!
+rm(iv.big,iv.beta,iv.se,iv.t,iv.rej,iv.f)
+
 
