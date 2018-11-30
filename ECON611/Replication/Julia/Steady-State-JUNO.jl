0f076e8b4509771fe24e244621ff19aedf781db9
Ani <asyadav@umich.edu>
2018-11-30 17:02:04 -0500

    Update Steady-State-JUNO.jl

--- ECON611/Replication/Julia/Steady-State-JUNO.jl
+++ ECON611/Replication/Julia/Steady-State-JUNO.jl
@@ -36,17 +36,17 @@ b_grid = collect(linspace(b_min,b_max,200));
 # EGM function
 ######################################################################
 
-function egm(G::AbstractArray,
-             b_grid::AbstractArray,
-             z_grid::AbstractArray,
-             P::AbstractArray,
-             β::Real,
-             γ::Real,
-             ψ::Real,
-             R::Real,
-             W::Real,
-             D::Real,
-             τ::Real)
+function egm{TR<:Real,TF<:AbstractFloat}(G::Array{TF,2},
+             b_grid::Array{TF,1},
+             z_grid::Array{TF,1},
+             P::Array{Float64,2},
+             β::TR,
+             γ::TR,
+             ψ::TR,
+             R::TR,
+             W::TR,
+             D::TR,
+             τ::TR)
 
     # Allocate memory for value of today's consumption on endogenous grid points,...
     # and the endogenous grid itself,...
@@ -96,16 +96,15 @@ end
 # Initial guess of policy function
 ######################################################################
 
-function G_init{TR<:Real}(D::TR,τ::TR)
+function G_init{TR<:Real}(W::Real,D::TR,τ::TR)
 
     G0   = zeros(length(z_grid),length(b_grid))
 
     for (i,z) in enumerate(z_grid)
             for(j,b) in enumerate(b_grid)
-                G0[i,j] = (b_grid[j]+D-τ*tax(z_grid[i])+sqrt((b_grid[j]+D-τ*tax(z_grid[i]))^2+4*(Wbar*z_grid[i])^(1+1/ψ)))/2
+                G0[i,j] = (b_grid[j]+D-τ*tax(z_grid[i])+sqrt((b_grid[j]+D-τ*tax(z_grid[i]))^2+4*(W*z_grid[i])^(1+1/ψ)))/2
             end
     end
-
     return G0
 
 end
@@ -114,12 +113,12 @@ end
 # Compute policy function
 ######################################################################
 
-function get_policy{TR<:Real}(G0, D::TR, τ::TR, tol=1e-8, maxiter=1000, err=1, i=0)
+function get_policy{TR<:Real}(G0,R::TR,W::TR, D::TR, τ::TR, tol=1e-8, maxiter=1000, err=1, i=0)
     G       = G0    # Initial guess
 
     while i<=maxiter && err > tol
 
-        Gnew = egm(G,b_grid,z_grid,MC.p,β,γ,ψ,Rbar,Wbar,D,τ)
+        Gnew = egm(G,b_grid,z_grid,MC.p,β,γ,ψ,R,W,D,τ)
 
         err  = maximum(abs, Gnew - G)
         G    = Gnew
@@ -168,7 +167,7 @@ end
 Z=mc_sample_path(MC.p,2,1000000)
 
 # Function to get associated bond holdings, using policy function
-function get_bonds{TR<:Real}(Z,G::AbstractArray,D::TR,τ::TR,c1,c2,c3,init=b_grid[1])
+function get_bonds{TR<:Real}(Z,G::AbstractArray,R::Real,W::Real,D::TR,τ::TR,c1,c2,c3,init=b_grid[1])
     B = zeros(length(Z)) #allocate memory
 
     # This is what c1,c2,c3 will be!
@@ -181,11 +180,11 @@ function get_bonds{TR<:Real}(Z,G::AbstractArray,D::TR,τ::TR,c1,c2,c3,init=b_gri
     for t in 1:(length(B)-1)
 
         if Z[t]==z_grid[1]
-            B[t+1]=Rbar*(B[t]+(Wbar*Z[t])^(1+1/ψ)*c1(B[t])^(-γ/ψ)-c1(B[t])+D-τ*tax(z_grid[1]))
+            B[t+1]=R*(B[t]+(W*Z[t])^(1+1/ψ)*c1(B[t])^(-γ/ψ)-c1(B[t])+D-τ*tax(z_grid[1]))
         elseif Z[t]==z_grid[2]
-            B[t+1]=Rbar*(B[t]+(Wbar*Z[t])^(1+1/ψ)*c2(B[t])^(-γ/ψ)-c2(B[t])+D-τ*tax(z_grid[2]))
+            B[t+1]=R*(B[t]+(W*Z[t])^(1+1/ψ)*c2(B[t])^(-γ/ψ)-c2(B[t])+D-τ*tax(z_grid[2]))
         else
-            B[t+1]=Rbar*(B[t]+(Wbar*Z[t])^(1+1/ψ)*c3(B[t])^(-γ/ψ)-c3(B[t])+D-τ*tax(z_grid[3]))
+            B[t+1]=R*(B[t]+(W*Z[t])^(1+1/ψ)*c3(B[t])^(-γ/ψ)-c3(B[t])+D-τ*tax(z_grid[3]))
         end
     end
 
@@ -216,7 +215,9 @@ end
 ######################################################################
 #function solve_ss(D=0,tol=1e-8,maxiter=1000,err=1,i=0)
 
-D       = 0    # Initial guess
+W       = Wbar   # Initial guess
+R       = Rbar
+D       = 0
 τ       = 0
 tol     = 1e-8
 maxiter = 1000
@@ -226,30 +227,46 @@ i       = 0
 
     while i<=maxiter && err > tol
 
-        global G, C, D, B, τ             # Ensures the loop spits out these things (cf. local)
+        global G, W, R, C, D, B, τ       # Ensures the loop spits out these things (cf. local)
 
-        X    = [D,τ]
+        X    = [R,W,D,τ]
 
-        G0   = G_init(D,τ)               # Compute initial guess of policy fn
+        G0   = G_init(W,D,τ)             # Compute initial guess of policy fn
 
-        G    = get_policy(G0,D,τ)        # Compute policy function
+        G    = get_policy(G0,R,W,D,τ)    # Compute policy function
 
         c1 = LinInterp(b_grid,G[1,:])    # optimal policy *function* for low productivity
         c2 = LinInterp(b_grid,G[2,:])
         c3 = LinInterp(b_grid,G[3,:])
 
-        B  = get_bonds(Z,G,D,τ,c1,c2,c3) # Simulate distribution of bonds
+        B  = get_bonds(Z,G,R,W,D,τ,c1,c2,c3) # Simulate distribution of bonds
 
         C  = get_C(B,c1,c2,c3)           # Get consumption distribution
 
-        Dnew = mean(C)*(1-Wbar)          # Compute prelim updated guess of divs
+        # CHECK LABOR MARKET CLEARING AND ADJUST WAGES
+        L    = C.^(-γ/ψ).*((W*Z).^(1/ψ)); # Compute lsupply using intratemporal sub condition
+        zL   = L.*Z;
+        Lbar = mean(zL)
 
-        τnew = 1.4*4*mean(C)*(Rbar-1)/Rbar # Compute prelim updated guess of τ
+        Wtil = W + 0.8*(mean(C)-Lbar)
 
-        Xnew = [Dnew,τnew]
+        Wnew = 0.5*Wtil + 0.5*W
+
+        # CHECK BOND MARKET CLEARING AND ADJUST INTEREST RATE
+        Rtil = R - 0.001*(mean(B)-1.4*4*mean(C))
+
+        Rnew = 0.5*Rtil + 0.5*R
+
+        Dnew = mean(C)*(1-W)             # Compute prelim updated guess of divs
+
+        τnew = 1.4*4*mean(C)*(Rnew-1)/Rnew     # Compute prelim updated guess of τ
+
+        Xnew = [Rnew,Wnew,Dnew,τnew]
 
         err  = maximum(abs, Xnew - X)  # Compute error
 
+        R    = Rnew
+        W    = Wnew
         D    = 0.5*Dnew + 0.5*D        # Update guess of dividends
         τ    = 0.5*τnew + 0.5*τ         # Update guess of τ
 
