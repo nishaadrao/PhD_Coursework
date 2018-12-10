e62b3915d387dc5b84c53f75f6d563247891ccba
Ani <asyadav@umich.edu>
2018-12-09 21:15:19 -0500

    yupdates

--- ECON641/HW2/Q1-Levchenko.R
+++ ECON641/HW2/Q1-Levchenko.R
@@ -23,7 +23,7 @@ setwd("/Users/Anirudh/Desktop/GitHub/PhD_Coursework/ECON641/HW2")
 data = as.data.table(read.dta13('PanelAnnual_compustat1980_2015.dta'))
 
 # Create 1 digit SIC codes
-data[,sic1:=cut(sic,c(0,999,1999,3999,4999,5999,6999,8999,9999),labels=c("Ag","MinCon","Man","Tran","WRTrade","Fin","Serv","Pub"))]
+data[,sic1:=cut(sic,c(0,999,1999,3999,4999,5999,6999,8999,9999),labels=c("Agriculture","Mining + Construction","Manufacturing","Transport, etc.","Wholesale + Retail Trade","Finance, etc.","Services","Public Admin"))]
 
 # Create additional variables, by fyear
 data[,`:=`(rsales=rank(-sale), remp=rank(-emp), logsale=log(sale),logemp=log(emp)), by=fyear]
@@ -65,10 +65,13 @@ p2015.all.ind = ggplot(data[fyear==2015],aes(x=logsale,y=log(rsales.ind)))+
                  xlab("log(Sales)")+ylab("log(Rank)") + facet_wrap(~sic1)
 
 # Plot 5 -- top 100 firms, by industry
-p2015.all.ind = ggplot(data[fyear==2015 & rsales.ind<=100],aes(x=logsale,y=log(rsales.ind)))+
+p2015.100.ind = ggplot(data[fyear==2015 & rsales.ind<=100],aes(x=logsale,y=log(rsales.ind)))+
                   geom_point()+
                   xlab("log(Sales)")+ylab("log(Rank)") + facet_wrap(~sic1)
 
+p2015.100.ind.ex = ggplot(data[fyear==2015 & rsales.ind<=100 & sic1!="Agriculture" & sic1!="Public Admin"],aes(x=logsale,y=log(rsales.ind)))+
+                    geom_point()+
+                    xlab("log(Sales)")+ylab("log(Rank)") + facet_wrap(~sic1)
 
 ######################################################################
 # Power law coefficients -- SALES
@@ -92,4 +95,21 @@ sales.gabaixSE = sapply(1:3, function(i) sqrt(2/cutoffs[i])*abs(sales.betas[i]))
 # Combind results
 sales.results = rbind(sales.betas,sales.gabaixSE)
 colnames(sales.results) = c("Full", "Top 500", "Top 100")
+
+
+######################################################################
+# Power law coefficients -- SALES -- BY INDUSTRY
+######################################################################
+
+cutoffs.ind = c(length(data[fyear==2015,rsales]),100,50)
+ind.vec = c("Agriculture","Mining + Construction","Manufacturing","Transport, etc.","Wholesale + Retail Trade","Finance, etc.","Services","Public Admin")
+
+# Run OLS regs -- spits out a big list of 3 x 8 x 12 (i.e. 24 regressions)
+sales.regs.ind = foreach(j=ind.vec,.combine='cbind')%:%
+                  foreach(i=cutoffs.ind, .combine = 'c') %do%{
+                   lm(log(rsales.ind-0.5)~logsale,data[fyear==2015 & rsales.ind<=i & logsale!=-Inf & sic1==j])
+                }
+
+#lapply(1:3,function(i) lm(log(rsales.ind-0.5)~logsale,data[fyear==2015 & rsales.ind<=cutoffs[i] & logsale!=-Inf] & sic1=ind.vec[j]))
+
 
